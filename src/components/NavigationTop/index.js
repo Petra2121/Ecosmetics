c5dded0281eadae68489845a4e8dae974943c0a1
mihaela-w <mwang00@fesb.hr>
2020-12-05 19:38:46 +0100

    Adding UserMenu and Icons

--- src/components/NavigationTop/index.js
+++ src/components/NavigationTop/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Logo from '../Logo'
+import UserMenu from '../UserMenu'
 
 import styles from './style.module.css'
 
 const Navigation = () => (
-  <section>
+  <section className={styles.navigationTop}>
     <Logo />
+    <UserMenu />
   </section>
 )
 
