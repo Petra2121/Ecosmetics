a3b094c19eda31a43518288705f440d752398665
Petra2121 <petra21lovric@gmail.com>
2020-12-05 20:06:19 +0100

    Adding Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Footer = () => (
+    <footer className={styles.footer} >
+        <ul className={styles.footertext}>
+            <li className={styles.title}>
+                Ecosmetics © 2021 FESB
+            </li>
+            <li>Petra Lovrić, Mihaela Wang</li>
+        </ul>
+        <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="47" viewBox="0 0 46 47">
+        <path id="Union_1" data-name="Union 1" d="M0,47V41.126c0-6.464,10.35-11.751,23-11.751s23,5.287,23,11.751V47ZM11.5,11.751A11.628,11.628,0,0,1,23,0,11.628,11.628,0,0,1,34.5,11.751,11.627,11.627,0,0,1,23,23.5,11.627,11.627,0,0,1,11.5,11.751Z" fill="#020202"/>
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="59" viewBox="0 0 46 59">
+        <g id="shopping_bag-24px" transform="translate(0.302 0.471)">
+        <rect id="Rectangle_746" data-name="Rectangle 746" width="46" height="59" transform="translate(-0.302 -0.471)" fill="none"/>
+        <path id="Path_209" data-name="Path 209" d="M40.385,12.462h-5.2a10.4,10.4,0,1,0-20.792,0H9.2A5.23,5.23,0,0,0,4,17.694V49.081a5.23,5.23,0,0,0,5.2,5.231H40.385a5.23,5.23,0,0,0,5.2-5.231V17.694A5.23,5.23,0,0,0,40.385,12.462ZM24.792,7.231a5.23,5.23,0,0,1,5.2,5.231h-10.4A5.23,5.23,0,0,1,24.792,7.231Zm15.594,41.85H9.2V17.694h5.2v5.231a2.6,2.6,0,1,0,5.2,0V17.694h10.4v5.231a2.6,2.6,0,1,0,5.2,0V17.694h5.2Z" transform="translate(-4 2.511)"/>
+        </g>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="59" viewBox="0 0 46 59">
+        <g id="shopping_bag-24px" transform="translate(0.302 0.471)">
+        <rect id="Rectangle_746" data-name="Rectangle 746" width="46" height="59" transform="translate(-0.302 -0.471)" fill="none"/>
+        <path id="Path_209" data-name="Path 209" d="M40.385,12.462h-5.2a10.4,10.4,0,1,0-20.792,0H9.2A5.23,5.23,0,0,0,4,17.694V49.081a5.23,5.23,0,0,0,5.2,5.231H40.385a5.23,5.23,0,0,0,5.2-5.231V17.694A5.23,5.23,0,0,0,40.385,12.462ZM24.792,7.231a5.23,5.23,0,0,1,5.2,5.231h-10.4A5.23,5.23,0,0,1,24.792,7.231Zm15.594,41.85H9.2V17.694h5.2v5.231a2.6,2.6,0,1,0,5.2,0V17.694h10.4v5.231a2.6,2.6,0,1,0,5.2,0V17.694h5.2Z" transform="translate(-4 2.511)"/>
+        </g>
+        </svg>
+        </div>
+    </footer>
+  )
+ 
+  export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Footer from "../components/Footer"
 
 import NavigationHeader from '../components/NavigationHeader'
 
 const IndexPage = () => (
   <main>
    <NavigationHeader />
+   <Footer />
   </main>
 )
 
