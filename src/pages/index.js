0d0a3d3c8355cf6ce904f7e5b7c7b32e3e524218
Petra2121 <petra21lovric@gmail.com>
2020-12-05 14:29:05 +0100

    Adding NavigationHeader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from "react"
 
-import SEO from "../components/seo"
+import NavigationHeader from '../components/NavigationHeader'
 
 const IndexPage = () => (
   <main>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>This is were we will add content</p>
+   <NavigationHeader />
   </main>
 )
 
 export default IndexPage
+
