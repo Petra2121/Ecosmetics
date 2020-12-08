03ce27cb8e9fe1489ab256d9ea59e4fcea393316
mihaela-w <mwang00@fesb.hr>
2020-12-08 14:59:11 +0100

    added font, color and ul to NavigatonBar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -4,10 +4,12 @@ import styles from './style.module.css'
 
 const NavigationBar = () => (
   <nav className={styles.navigationBar}>
+    <ul className={styles.navigationButtons}>
       <li className={styles.active}>Home</li>
       <li>Products</li>
       <li>Brands</li>
       <li>Blog</li>
+    </ul>
   </nav>
 )
 
