63cb7a2c86dd7decbf74b8ca4a62877228ba6e12
mihaela-w <maite.miwa@gmail.com>
2020-12-20 21:20:08 +0100

    Added text in OurProducts

--- src/components/OurProducts/index.js
+++ src/components/OurProducts/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import OurProductsBox from './OurProductsBox'
 
 import styles from './style.module.css'
  
 const OurProducts = () => (
     <section className={styles.ourProducts}>
-
+        <div className={styles.leftText}>
+            <span>OUR PRODUCTS</span>
+            <p>While the performance of natural makeup used to pale 
+                in comparison to products from our favorite makeup 
+                counters, there are many natural makeup brands creating 
+                products that are richly pigmented, apply beautifully, 
+                and wear as well as their more synthetic competitors.</p>
+        </div>
+        <div className={styles.boxes}>
+            <OurProductsBox/>
+        </div>
     </section>
 )
  
