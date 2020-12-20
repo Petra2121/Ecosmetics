b1ed5721948363ecb5c7d0cacdbbc985e3cdc87a
mihaela-w <maite.miwa@gmail.com>
2020-12-20 19:14:53 +0100

    Added components for OurProducts

---
+++ src/components/OurProducts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './style.module.css'
+ 
+const OurProducts = () => (
+    <section className={styles.ourProducts}>
+
+    </section>
+)
+ 
+export default OurProducts
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import HeroImage from "../components/HeroImage"
- 
+import OurProducts from "../components/OurProducts"
 import HeaderFooterLayout from "../layouts/headerFooter"
  
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     <HeroImage/>
+    <OurProducts/>
   </HeaderFooterLayout>
 )
  
