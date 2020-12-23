453d0519c8b44bfee74673a77e7f7ef8d5813308
mihaela-w <maite.miwa@gmail.com>
2020-12-23 14:23:13 +0100

    Added HeroImage as gatsby-background-img

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 import styles from './style.module.css'
  
-const HeroImage = () => (
-    <section className={styles.heroImage}>
-        <div>
-            <p>Ovdi ce ic onaj tekst priko slike</p>
-        </div>
-    </section>
+const HeroImage = () => {
+    const data = useStaticQuery(graphql`
+    query{
+        desktop: file(relativePath: {eq: "hero-image.jpg"}) {
+            childImageSharp{
+                fluid(quality: 90, maxWidth: 1920){
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+            }
+        }
+    }
+    `)
+    return (
+        <BackgroundImage
+        Tag="section"
+        className={styles.heroImage}
+        fluid={data.desktop.childImageSharp.fluid}
+        backgroundColor={`#040e18`}
+        >
+            <div>
+                <p>Ovdi ce ic onaj tekst priko slike</p>
+            </div>
+        </BackgroundImage>
 )
+    }
  
 export default HeroImage
