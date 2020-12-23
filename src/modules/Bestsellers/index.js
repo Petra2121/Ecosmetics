9fe9f250d832f270d3d30ee4602c944ff99f1d91
mihaela-w <maite.miwa@gmail.com>
2020-12-23 22:40:31 +0100

    Finished Bestsellers Module

--- src/modules/Bestsellers/index.js
+++ src/modules/Bestsellers/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ProductCard from '../../components/ProductCard'
+
 import styles from './style.module.css'
+import { GoChevronLeft } from "@react-icons/all-files/go/GoChevronLeft";
+import { GoChevronRight } from "@react-icons/all-files/go/GoChevronRight";
 
 const Bestsellers = () => (
   <section className={styles.bestsellers}>
-    <ProductCard/>
+    <span  className={styles.bestsellersTitle}>Bestsellers</span>
+
+    <div className={styles.container}>
+      <GoChevronLeft className={styles.icon1}/>
+      <div className={styles.products}>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
+      <GoChevronRight className={styles.icon2}/>
+     </div>
+
   </section>
 )
 
