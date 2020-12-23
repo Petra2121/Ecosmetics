1bc7f9a937b26fd156f79640d895c2abb14b1a5e
mihaela-w <maite.miwa@gmail.com>
2020-12-23 13:47:01 +0100

    Added text as constant in OurProducts

--- src/components/OurProducts/index.js
+++ src/components/OurProducts/index.js
@@ -2,16 +2,17 @@ import React from 'react'
 import OurProductsBox from './OurProductsBox'
 
 import styles from './style.module.css'
+
+const text = `
+While the performance of natural makeup used to pale in comparison to products from our favorite makeup 
+counters, there are many natural makeup brands creating products that are richly pigmented, apply beautifully, 
+and wear as well as their more synthetic competitors.`
  
 const OurProducts = () => (
     <section className={styles.ourProducts}>
         <div className={styles.leftText}>
             <span>Our products</span>
-            <p>While the performance of natural makeup used to pale 
-                in comparison to products from our favorite makeup 
-                counters, there are many natural makeup brands creating 
-                products that are richly pigmented, apply beautifully, 
-                and wear as well as their more synthetic competitors.</p>
+            <p>{text}</p>
         </div>
         <div className={styles.boxes}>
             <OurProductsBox/>
