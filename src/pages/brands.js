9ed2297ddb9a93909e2d7934aa36dc1e9e364ca6
Petra2121 <petra21lovric@gmail.com>
2020-12-29 21:30:55 +0100

    Adding MeetOurBrands text above BrandCards

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,9 +1,11 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import BrandCardParagraph from "../modules/BrandCardParagraph"
+import MeetBrands from "../components/MeetOurBrands"
  
 const BrandsPage = () => (
     <HeaderFooterLayout activeTab="Brands">
+        <MeetBrands />
         <BrandCardParagraph />
     </HeaderFooterLayout>
 )
