425cae3256969891105d4af9256e92e40a3be270
Petra2121 <petra21lovric@gmail.com>
2020-12-30 19:32:23 +0100

    ProductThreeImages without real graphQL, just layout

--- src/components/ProductThreeImages/index.js
+++ src/components/ProductThreeImages/index.js
@@ -1,11 +1,70 @@
 import React from 'react'
 import styles from './style.module.css'
 
-const ProductThreeImages = () => (
-    <section className={styles.imagesSection}>
-  
-  
-    </section>
+import { useStaticQuery, graphql } from "gatsby"
+// import Img from "gatsby-image"
+// import { Link } from 'gatsby'
+
+import image1 from '../../images/strawberry-shampoo1.jpg'
+import image2 from '../../images/strawberry-shampoo2.jpg'
+import image3 from '../../images/strawberry-shampoo3.jpg'
+
+const ProductThreeImages = () => {
+  const data = useStaticQuery(graphql`
+    query ClickedProduct {
+      productsJson(name: {eq: "Strawberry shower gel"}) {
+        alt
+        category
+        details
+        img {
+          childImageSharp {
+            fluid (maxWidth: 250) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        img2 {
+          childImageSharp {
+            fluid (maxWidth: 250) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        img3 {
+          childImageSharp {
+            fluid (maxWidth: 250) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        ingredients
+        name
+        price
+      }
+    }    
+    
+  `)
+
+  return (
+    <div className={styles.maindiv}>
+      <div className={styles.twopictures}>
+        <img src={image2} className={styles.img2} />
+        <img src={image3} className={styles.img3}/>
+      </div>
+
+      <div className={styles.rightpicture}>
+        <img src={image1} className={styles.img1} />
+      </div>
+
+      <div className={styles.informationpart}>
+        <span className={styles.name}>Strawberry shower gel</span>
+        <span className={styles.number}>16$</span>
+        <button className={styles.button}>Add to bag</button>
+      </div>
+
+    </div>
   )
   
+}
   export default ProductThreeImages
+
--- src/pages/product.js
+++ src/pages/product.js
@@ -1,8 +1,10 @@
 import React from "react"
+import ProductThreeImages from "../components/ProductThreeImages"
 import HeaderFooterLayout from "../layouts/headerFooter"
  
 const ProductPage = () => (
     <HeaderFooterLayout activeTab="Products">
+        <ProductThreeImages />
     </HeaderFooterLayout>
 )
  
