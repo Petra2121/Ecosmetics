395719ba66d504ce9ec8019e4bf573842fd97ecc
mihaela-w <maite.miwa@gmail.com>
2021-01-05 15:03:59 +0100

    Added HeroImage for Brands

---
+++ src/modules/HeroImageBrands/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+
+import styles from './style.module.css'
+ 
+const text = `
+We can change the world together`
+
+const HeroImageBrands = () => {
+    const data = useStaticQuery(graphql`
+    query{
+        desktop: file(relativePath: {eq: "hero-image-brands.jpg"}) {
+            childImageSharp{
+                fluid(quality: 90, maxWidth: 1920){
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+            }
+        }
+    }
+    `)
+    return (
+        <BackgroundImage
+        Tag="section"
+        className={styles.heroImageBrands}
+        fluid={data.desktop.childImageSharp.fluid}
+        backgroundColor={`#040e18`}
+        >
+            <div>
+                <p>&#8195;&#8195;{text}</p>
+            </div>
+        </BackgroundImage>
+)
+    }
+ 
+export default HeroImageBrands
--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,10 +1,12 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
+import HeroImageBrands from "../modules/HeroImageBrands"
 import MeetBrands from "../components/MeetOurBrands"
 import BrandCard from "../components/BrandCard"
  
 const BrandsPage = () => (
     <HeaderFooterLayout activeTab="Brands">
+        <HeroImageBrands/>
         <MeetBrands />
         <BrandCard />
     </HeaderFooterLayout>
