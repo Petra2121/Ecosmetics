b9c88edfae6423272c7996cae68628bd6a50e1ae
mihaela-w <maite.miwa@gmail.com>
2021-01-06 17:38:13 +0100

    Imported icons for mobile navbar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -3,6 +3,13 @@ import { navs as navTabs } from '../../constants/const'
 import styles from './style.module.css'
 import { Link } from 'gatsby'
 
+import {HiOutlineHome} from "@react-icons/all-files/hi/HiOutlineHome"
+import {RiInkBottleLine} from "@react-icons/all-files/ri/RiInkBottleLine"
+import {FaTree} from "@react-icons/all-files/fa/FaTree"
+import {BiComment} from "@react-icons/all-files/bi/BiComment"
+import { BsBag } from "@react-icons/all-files/bs/BsBag";
+
+
 const NavigationBar = ({activeTab}) => (
   <nav className={styles.navigationBar}>
         {navTabs.map(({tab, to}) => (
@@ -10,6 +17,12 @@ const NavigationBar = ({activeTab}) => (
               <li className={tab === activeTab ? styles.active : ''}>{tab}</li>
           </Link>)
         )}
+
+        <HiOutlineHome className={styles.icon}/>
+        <RiInkBottleLine className={styles.icon}/>
+        <FaTree className={styles.icon}/>
+        <BiComment className={styles.icon}/>
+        <BsBag className={styles.icon}/> 
   </nav>
 )
 
