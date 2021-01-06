ce7d392e3ef0c64a91f5da4b4e614c8fa2b4b5b2
mihaela-w <maite.miwa@gmail.com>
2021-01-06 17:00:00 +0100

    HeroImg for first Blogpost

--- src/pages/topic.js
+++ src/pages/topic.js
@@ -1,8 +1,10 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
+import HeroImageBlogpost from "../modules/HeroImageBlogpost"
  
 const TopicPage = () => (
     <HeaderFooterLayout activeTab="Blog">
+        <HeroImageBlogpost/>
     </HeaderFooterLayout>
 )
  
