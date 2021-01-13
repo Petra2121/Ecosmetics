3552f11ae1114616b80e4cbccf7081a2a16c2664
mihaela-w <maite.miwa@gmail.com>
2021-01-13 12:13:29 +0100

    Adding useState to UserMenu

--- src/components/UserMenu/index.js
+++ src/components/UserMenu/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './style.module.css'
 
 import { FaUser } from "@react-icons/all-files/fa/FaUser";
 import { BsBag } from "@react-icons/all-files/bs/BsBag";
 
-const UserMenu = () => 
-<section className={styles.icons}>
-    <FaUser className={styles.icon}/>
-    <BsBag className={styles.icon}/>   
-</section>
+const UserMenu = () => {
+    const [showModal, setShowModal] = useState(false);
+    
+    const openModal = () => {
+        setShowModal(prev => !prev);
+    };
+
+    return (<section className={styles.icons}>
+        <FaUser className={styles.icon}/>
+        <BsBag className={styles.icon}/>   
+    </section>
+    )
+}
 
 export default UserMenu
