b2bef069bb94941afb1b789d621e9ed8b63ca0fc
mihaela-w <maite.miwa@gmail.com>
2021-01-13 12:27:49 +0100

    Adding LoginModal and helper.js

---
+++ src/components/LoginModal/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {myLocalStorage} from '../../components/helper'
+ 
+import styles from './style.module.css'
+
+const LoginModal = () => (
+    <section className={styles.loginModal}>
+      Login
+    </section>
+  )
+  
+  export default LoginModal
