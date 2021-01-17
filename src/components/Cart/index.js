040fbcd88acfc42865fbdafd43051111a4e2194d
mihaela-w <maite.miwa@gmail.com>
2021-01-17 16:05:24 +0100

    CSS for Cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import styles from './style.module.css'
+import {BsTrash} from '@react-icons/all-files/bs/BsTrash'
+import shampoo from '../../images/products/artisan-soap1.jpg'
 
 const Cart = ({showModalCart, setShowModalCart}) => {
     return (
         <>
         {showModalCart?(
   <section className={styles.cart} showModalCart={showModalCart}>
-    <div></div>
-    <div>
-        <span>My bag (1)</span>
-        <hr className={styles.line}></hr>
-        <div>
-            <img/>
-            <div>
-                <span>Strawberry glossing shampoo</span>
-                <div>
-                    <span>16$</span>
-                    <div>
-                        <span>1</span>
-                        {/* ikona */}
+    <div className={styles.brownTop}></div>
+    <div className={styles.wrapper}>
+        <div className={styles.whiteBottom}>
+            <span className={styles.myBag}>My bag (1)</span>
+            <hr className={styles.line}></hr>
+            <div className={styles.product}>
+                <img className={styles.productImg} src={shampoo}/>
+                <div className={styles.rightOfImg}>
+                    <span className={styles.productName}>Artisan soap</span>
+                    <div className={styles.productInfo}>
+                        <span className={styles.productPrice}>9$</span>
+                        <div className={styles.qtyIcon}>
+                            <span className={styles.productQty}>1</span>
+                            <BsTrash className={styles.icon}/>
+                        </div>
                     </div>
                 </div>
             </div>
+            <hr className={styles.line}></hr>
+            <div className={styles.promoCodeDiv}>
+                <span className={styles.promoCode}>Promo code</span>
+                <div className={styles.promoCodeLine}></div>
+                <button className={styles.promoCodeBtn}>Add</button>
+            </div>
+            <hr className={styles.line}></hr>
+            <span className={styles.total}>Total 9$</span>
+            <button className={styles.paymentBtn}>Proceed to payment</button>
         </div>
-        <hr className={styles.line}></hr>
-        <div>
-            <span>Promo code</span>
-            <div></div>
-            <button></button>
-        </div>
-        <hr className={styles.line}></hr>
-        <span>Total 500$</span>
-        <button>Proceed to payment</button>
     </div>
   </section>
     ): null}
