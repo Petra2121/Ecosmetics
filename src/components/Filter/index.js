ecff3e275712411950285f362d110e65829aaace
Petra2121 <petra21lovric@gmail.com>
2021-01-17 01:02:11 +0100

    Filters for mobile version

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -2,17 +2,25 @@ import React from 'react'
 import styles from './style.module.css'
 
 const Filter = () => (
+  <div className={styles.wrapper}>
   <section className={styles.filter}>
+    <div className={styles.filterBy}>
     <span className={styles.textBold}>FILTER BY</span>
     <button className={styles.buttonTrans}>face</button>
     <button className={styles.buttonTrans}>body</button>
     <button className={styles.buttonTrans}>hair</button>
     <button className={styles.buttonTrans}>makeup</button>
+    </div>
+    <div className={styles.sortBy}>
     <span className={styles.textBold}>SORT BY</span>
     <button className={styles.buttonTrans}>price low to high</button>
     <button className={styles.buttonTrans}>price high to low</button>
-    <button className={styles.buttonApply}>Apply filters</button>
+    </div>
   </section>
+  <div className={styles.buttonWrap}>
+  <button className={styles.buttonApply}>Apply filters</button>
+  </div>
+  </div>
 )
 
 export default Filter
--- src/modules/ProductsAndFilter/index.js
+++ src/modules/ProductsAndFilter/index.js
@@ -3,14 +3,24 @@ import AllProducts from '../../components/AllProducts'
 import Filter from '../../components/Filter'
 import styles from './style.module.css'
 
+function showMask() {
+  var node = document.getElementById('mask123')
+  var display = node.style.display;
+  node.style.display = display == "block" ? 'none' : "block"
+}
+
 const ProductsAndFilter = () => <section className={styles.productFilter}>
+
     <div className={styles.filter}>
       <Filter />
     </div>
+    <button className={styles.openFilter} onClick={showMask}>filter and sort</button>
+      <div id="mask123" className={styles.mobFilter} style={{display: 'none'}}>
+        <Filter />
+      </div>
     <div className={styles.products}>
         <AllProducts />
     </div>
-    
 </section>
- 
+
 export default ProductsAndFilter
