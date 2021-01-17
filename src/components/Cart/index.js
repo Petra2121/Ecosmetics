70ef26e2cc46d14272afa4dac8bfa6c56f32675a
mihaela-w <maite.miwa@gmail.com>
2021-01-17 13:56:29 +0100

    HTML for Cart modal

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -3,7 +3,33 @@ import styles from './style.module.css'
 
 const Cart = () => (
   <section className={styles.cart}>
-    
+    <div></div>
+    <div>
+        <span>My bag (1)</span>
+        <hr className={styles.line}></hr>
+        <div>
+            <img/>
+            <div>
+                <span>Strawberry glossing shampoo</span>
+                <div>
+                    <span>16$</span>
+                    <div>
+                        <span>1</span>
+                        {/* ikona */}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr className={styles.line}></hr>
+        <div>
+            <span>Promo code</span>
+            <div></div>
+            <button></button>
+        </div>
+        <hr className={styles.line}></hr>
+        <span>Total 500$</span>
+        <button>Proceed to payment</button>
+    </div>
   </section>
 )
 
