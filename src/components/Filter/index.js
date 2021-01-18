94c52a0f98a7888f9490c4d98bd4205c28b860d2
Petra2121 <petra21lovric@gmail.com>
2021-01-18 13:05:10 +0100

    Filter changes in logic

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,33 +1,44 @@
 import React from 'react'
-
+ 
 import styles from './style.module.css'
-
+ 
+const myDocument = typeof document === 'undefined' ? {getElementById: (element) => {}} : {
+  getElementById: (element) => document.getElementById(element)
+}
+ 
+console.log(myDocument)
+ 
 const Filter = ({setFilters, appliedFilters, sort, setSort}) => {
-  const filtersToApply = []
-
+  const filtersToApply = [...appliedFilters]
+ 
   function showMask() {
-    var node = document.getElementById('mask123')
-    var display = node.style.display;
+    const node = myDocument.getElementById('mask123')
+    const display = node.style.display;
     node.style.display = display === "block" ? 'none' : "block"
   }
-
+ 
   function sorting1() {
-    var node = document.getElementById('lowtohigh')
-    
+    const node = myDocument.getElementById('lowtohigh')
+    const otherNode = myDocument.getElementById('hightolow')
+ 
     sort === "ASC" ? (node.style.backgroundColor="transparent") : (node.style.backgroundColor="#CFC5C5")
     sort === "ASC" ? setSort(null) : (setSort("ASC"))
+    otherNode.style.backgroundColor = "transparent"
   }
-
+ 
   function sorting2() {
-    var node = document.getElementById('hightolow')
+    const node = myDocument.getElementById('hightolow')
+    const otherNode = myDocument.getElementById('lowtohigh')
+ 
     sort === "DEC" ? (node.style.backgroundColor="transparent") : (node.style.backgroundColor="#CFC5C5")
     sort === "DEC" ? setSort(null) : setSort("DEC")
+    otherNode.style.backgroundColor = "transparent"
   }
-
+ 
   const toggleFilter = filterName => {
     // filtersToApply.push(filterName)
-    var node = document.getElementById(filterName)
-
+    var node = myDocument.getElementById(filterName)
+ 
     if (filtersToApply.includes(filterName)){
       const indexToRemove = filtersToApply.indexOf(filterName)
       if (indexToRemove !== -1) {
@@ -39,17 +50,17 @@ const Filter = ({setFilters, appliedFilters, sort, setSort}) => {
       node.style.backgroundColor = "#CFC5C5"
     }
   }
-
+ 
   return (
     <div className={styles.huge}>
       <div className={styles.wrapper}>
     <section className={styles.filter}>
       <div className={styles.filterBy}>
       <span className={styles.textBold}>FILTER BY</span>
-      <button id="Face" className={styles.buttonTrans} onClick={() => toggleFilter("Face")}>face</button>
-      <button id="Body" className={styles.buttonTrans} onClick={() => toggleFilter("Body")}>body</button>
-      <button id="Hair" className={styles.buttonTrans} onClick={() => toggleFilter("Hair")}>hair</button>
-      <button id="Makeup" className={styles.buttonTrans}  onClick={() => toggleFilter("Makeup")}>makeup</button>
+      <button id="Face" className={styles.buttonSelected} onClick={() => toggleFilter("Face")}>face</button>
+      <button id="Body" className={styles.buttonSelected} onClick={() => toggleFilter("Body")}>body</button>
+      <button id="Hair" className={styles.buttonSelected} onClick={() => toggleFilter("Hair")}>hair</button>
+      <button id="Makeup" className={styles.buttonSelected}  onClick={() => toggleFilter("Makeup")}>makeup</button>
       </div>
       <div className={styles.sortBy}>
       <span className={styles.textBold}>SORT BY</span>
@@ -61,17 +72,17 @@ const Filter = ({setFilters, appliedFilters, sort, setSort}) => {
     <button className={styles.buttonApply} onClick={() => setFilters([...filtersToApply])}>Apply filters</button>
     </div>
     </div>
-
+ 
     <button className={styles.openFilter} onClick={showMask}>filter and sort ⯆</button>
       <div id="mask123" className={styles.mobFilter} style={{display: 'none'}}>
     <div className={styles.wrapper2}>
     <section className={styles.filter}>
       <div className={styles.filterBy2}>
       <span className={styles.textBold}>FILTER BY</span>
-      <button id="Face" className={styles.buttonTrans} onClick={() => toggleFilter("Face")}>face</button>
-      <button id="Body" className={styles.buttonTrans} onClick={() => toggleFilter("Body")}>body</button>
-      <button id="Hair" className={styles.buttonTrans} onClick={() => toggleFilter("Hair")}>hair</button>
-      <button id="Makeup" className={styles.buttonTrans}  onClick={() => toggleFilter("Makeup")}>makeup</button>
+      <button id="Face" className={styles.buttonSelected} onClick={() => toggleFilter("Face")}>face</button>
+      <button id="Body" className={styles.buttonSelected} onClick={() => toggleFilter("Body")}>body</button>
+      <button id="Hair" className={styles.buttonSelected} onClick={() => toggleFilter("Hair")}>hair</button>
+      <button id="Makeup" className={styles.buttonSelected}  onClick={() => toggleFilter("Makeup")}>makeup</button>
       </div>
       <div className={styles.sortBy2}>
       <span className={styles.textBold}>SORT BY</span>
@@ -86,7 +97,7 @@ const Filter = ({setFilters, appliedFilters, sort, setSort}) => {
     </div>
     </div>
   )
-
+ 
 }
-
+ 
 export default Filter
